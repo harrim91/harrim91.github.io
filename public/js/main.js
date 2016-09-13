61f57eb5f7334e670a7005cfad4412740c3b857c
Michael Harrison <harrim91@hotmail.co.uk>
2016-09-13 15:27:49 +0100

    finished portfolio cards and skills, formatted blog posts

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 	new WOW().init();
+
+	$(document).on('click','.navbar-collapse.in',function(e) {
+		if( $(e.target).is('a') && 
+				$(e.target).attr('class') != 'dropdown-toggle' ) {
+			$(this).collapse('hide');
+		}
+	});
 });
