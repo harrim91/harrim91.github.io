d13b64bbb72af08317cb4ae4d7378527cadca5d2
Michael Harrison <harrim91@hotmail.co.uk>
2016-09-14 15:53:55 +0100

    added slick carousel

--- public/js/main.js
+++ public/js/main.js
@@ -8,5 +8,25 @@ $(document).ready(function() {
 		}
 	});
 
-	
+	$('.slick-portfolio').slick({
+		arrows: true,
+		prevArrow: $('.slick-prev'),
+		nextArrow: $('.slick-next'),
+		dots: false,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 7000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+
 });
