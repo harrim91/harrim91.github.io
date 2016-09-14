27643918aab3f75a36541e892a8f43eb4a59a91a
Michael Harrison <harrim91@hotmail.co.uk>
2016-09-14 14:19:40 +0100

    added contact and aboutsection

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	new WOW().init();
 
-	$(document).on('click','.navbar-collapse.in',function(e) {
+	$('.navbar-collapse').on('click',function(e) {
 		if( $(e.target).is('a') && 
 				$(e.target).attr('class') != 'dropdown-toggle' ) {
 			$(this).collapse('hide');
 		}
 	});
+
+	
 });
